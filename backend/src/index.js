3e4fb337faba48982d7b54e0c0bcd5949278ae82
Breno Soares <brenosoares4303@gmail.com>
2025-09-24 18:03:00 -0300

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const port = process.env.API_PORT || 3333;
+
+const app = express()
+
+const routes = require("./routes");
+
+app.use(express.json());
+app.use(cors());
+app.use("/api", routes);
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
